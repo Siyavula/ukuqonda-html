70b8c7d765199a51440e4b104179e3e850151dc0
Ewald Zietsman <ewald@siyavula.com>
2014-04-30 13:54:28 +0200

    Modified script

--- scripts/cleanup.py
+++ scripts/cleanup.py
@@ -2,7 +2,41 @@ import sys
 import os
 
 from lxml import etree
+import copy
 
+
+_elements_to_remove = []
+
+def replace_elements(element):
+    # replace some elements with <hr/> only.
+    hr = etree.Element('hr')
+    hr.tail = '\n'
+    if 'class' in element.attrib.keys():
+        if (element.tag == "div") and ("Basic-Text-Frame frame-" in element.attrib['class']):
+            if (element[0].tag == 'p'):
+                if element[0].attrib['class'] == "x-Answer-text-dotted-last-line--COPY-LINE-":
+                    #print("Adding hr")
+                    element.addprevious(copy.deepcopy(hr))
+                    _elements_to_remove.append(element)
+
+        for c in ["x--Answer-text-dotted-last-line para-style-override-", "x--Answer-text-dotted-last-line", "x--Answer-text-dotted-line-below"]:
+            if (element.tag == "p") and (c in element.attrib['class']):
+                if element.text is not None:
+                    if element.text.strip() in ["&#9;" ,"&#160;"]:
+                        element.addprevious(copy.deepcopy(hr))
+                        _elements_to_remove.append(element)
+
+                else:
+                    if len(element) == 1:
+                        if element[0].tag == 'span':
+                            if element[0].text == "&#160":
+                                element.addprevious(copy.deepcopy(hr))
+                                _elements_to_remove.append(element)
+
+
+
+    
+   
 
 
 if __name__ == "__main__":
@@ -10,4 +44,11 @@ if __name__ == "__main__":
 
     html = etree.HTML(open(filename, 'r').read())
 
-    print html
+    for e in html.iter():
+        replace_elements(e)
+
+    # remove the marked elements
+    for element in _elements_to_remove:
+        element.getparent().remove(element)
+
+    print(etree.tostring(html))
