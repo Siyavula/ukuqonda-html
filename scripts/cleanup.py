51a6be03b0acc84734299ddd3cb8167e59121907
Ewald Zietsman <ewald@siyavula.com>
2014-04-30 16:42:34 +0200

    Modified script

--- scripts/cleanup.py
+++ scripts/cleanup.py
@@ -32,11 +32,57 @@ def replace_elements(element):
                             _elements_to_remove.append(element)
 
 
+    if element.tag == 'span':
+        if 'class' in element.attrib.keys():
+            if 'Bodytext-Bold' in element.attrib['class']:
+                element.tag = 'b'
+                element.attrib['class'] = ''
+
+            elif 'Bodytext-Italic' in element.attrib['class']:
+                element.tag = 'i'
+                element.attrib['class'] = ''
+
+
+    # headers
+    if (element.tag == 'p'):
+        if element.attrib['class'] is not None:
+            if "Head-A" in element.attrib['class']:
+                element.tag = "h1"
+                del element.attrib['class']
+
+            elif "Head-B" in element.attrib['class']:
+                element.tag = "h2"
+                del element.attrib['class']
+
+            elif "Head-investigation--after-b-head-" in element.attrib['class']:
+                element.tag = 'h3'
+
+def delete_empty_elements(element):
+
+    elements_to_ignore = ['table', 'hr', 'td', 'col', 'img', 'figure', 'tbody', 'tr']
+
+    if element.tag not in elements_to_ignore:
+        text = "".join([t for t in element.itertext(with_tail=True)]).strip() 
+        if element.tail is not None:
+            text = text + element.tail.strip()
+        if text == "":
+            _elements_to_remove.append(element)
+
+    
+
+
+
+
 
 def string_replace(html_as_string):
 
     # multiplication
     html_as_string = html_as_string.replace("&#195;&#151;" ,"\\times")
+    html_as_string = html_as_string.replace("&#226;&#128;&#156;" ,'"')
+    html_as_string = html_as_string.replace("&#226;&#128;&#157;" ,'"')
+    html_as_string = html_as_string.replace("&#239;&#131;&#149;" ,'\\rightarrow')
+    html_as_string = html_as_string.replace(' xml:lang="en-US"' ,'')
+
 
 
     return html_as_string
@@ -50,7 +96,8 @@ if __name__ == "__main__":
 
     for e in html.iter():
         replace_elements(e)
-
+        delete_empty_elements(e)
+    
     # remove the marked elements
     for element in _elements_to_remove:
         parent = element.getparent()
